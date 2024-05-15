ea0cd9f3f43fd4c3024b00d53dc3a1d86e6cab62
Andrii Zabolotin <fenics7511@gmail.com>
2024-05-15 13:59:06 +0300

    Create settings class

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    db_url: str = Field(..., json_schema_extra={"env": "DB_URL"})
+    db_echo: bool = True
+
+
+settings = Settings()
